2fc9fd92e51b9c05e35e73fec30c6c519df9428e
imreg <vongeczyimre@dreams.co.uk>
2013-02-01 15:28:08 +0000

    Update config/environment.rb
    extended by a condition of version control

--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,17 @@
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
 
+if Gem:: VERSION   >= "1.3.6"
+     module Rails
+         class GemDependency
+             def requirement
+                 r = super
+                 (r == Gem::Requirement.default) ? nil : r
+             end
+         end
+     end
+end 
+
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
